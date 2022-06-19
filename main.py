5801dd631dc450f29a3e1164524b5b4e83b12e34
DineshTeja <51058492+DineshTeja@users.noreply.github.com>
2022-06-19 14:37:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -197,6 +197,7 @@ with col1:
 with col2:
   optimize = st.button("Optimize")
 
+
 if yes:
 
   #"""## **Define** Word Embedding/Doc2Vec Functionality Tools"""
@@ -477,3 +478,258 @@ if yes:
 
   #MAPE
   st.sidebar.metric(label = 'Mean Absolute Percentage Error', value = mean_absolute_percentage_error(final['preds'],final['results__maxExternalScore']) - 0.05)
+
+if optimize:
+  #"""## **Define** Word Embedding/Doc2Vec Functionality Tools"""
+
+  st.success('Define Word Embedding/Doc2Vec Functionality Tools')
+  logger = logging.getLogger(__name__)
+  logger.setLevel(logging.DEBUG)
+
+  # initialization 
+  wnl = WordNetLemmatizer()
+  ## ^ finds the root word from different forms of a word (should provide POS tag for efficiency)
+
+  tokenizer = RegexpTokenizer(r'\w+')
+  ## ^ tokenizes all the words in a string 
+
+  p_stemmer = PorterStemmer()
+  ## ^ shortens the lookup, and normalizes sentences.
+
+  #"""## Import and Prepare Data (finaloutput.csv)"""
+  st.success('Import and Prepare Data (finaloutput.csv)')
+
+  datapath = 'data/finaloutput.csv'
+  df = pd.read_csv(datapath)
+
+  train_df, test_df = train_test_split(df, test_size = 0.3, random_state=1)
+
+  import smart_open
+
+  def read_corpus(f, tokens_only=False):
+          for i, line in enumerate(f):
+              tokens = gensim.utils.simple_preprocess(line)
+              if tokens_only:
+                  yield tokens
+              else:
+                  # For training data, add tags
+                  yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+
+  train_corpus = list(read_corpus(train_df['results__description']))
+  test_corpus = list(read_corpus(test_df['results__description'], tokens_only=True))
+
+  #"""## Doc2Vec Model Creation and Training"""
+
+  st.success('Doc2Vec Model Creation and Training')
+
+  model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=40)
+
+  model.build_vocab(train_corpus)
+
+  model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+
+  #Define Function to Create Word Embeddings from Text Input
+  def generate_embeddings(text, model = model):
+      try:
+          text = text.lower()
+          tokens = tokenizer.tokenize(text)
+          tokens = [wnl.lemmatize(token) for token in tokens]
+          tokens = [p_stemmer.stem(token) for token in tokens] 
+          tokens = [i for i in tokens if len(i) >= 1]
+          model.random.seed(42)
+
+          vector = model.infer_vector(tokens)
+      except:
+          return -1
+      return vector
+
+  #"""## Appending Word Embeddings to Data File
+
+  #"""
+  st.success('Appending Word Embeddings to Data File')
+
+  #Append DOC2VEC Vectors to DF
+  final_corpus = list(read_corpus(df['results__description'], tokens_only= True))
+  column3 = []
+
+  for x in final_corpus:
+    y = model.infer_vector(x)
+    column3.append(y)
+
+  df['vector_item'] = column3
+  df.head()
+
+  #"""##Organizing Inputs and Creating Train/Test Dataframes"""
+
+  st.success('Organizing Inputs and Creating Train/Test Dataframes')
+  #Divide Vectors into 50 individual word embeddings (collectively represent descriptions)
+
+  bigList = []
+  for i in range(50):
+    bigList.append([])
+
+  for x in df['vector_item']:
+    for i in range(50):
+      bigList[i].append(x[i])
+    
+  for i in range(50):
+    df['column' + str(i)] = bigList[i]
+
+  X = []
+  for i in range(50):
+    X.append('column'+ str(i))
+    
+
+  X.append('results__hasExploit')
+  X.append('results__hasFix')
+
+  X = 'vector_item'
+  y = 'results__maxExternalScore'
+
+  train_df, test_df = train_test_split(df, test_size=0.3, random_state=0)
+  X_train = train_df[X]
+  y_train = train_df[y]
+  X_test = test_df[X]
+  y_test = test_df[y]
+
+  #"""##Defining Required Functions and Begin FastAi Training Process """
+  st.success('Defining Required Functions and Begin FastAi Training Process')
+
+  #-----------------------------------------------------------------xxx-----------------------------------------------------------------
+  # Read Data and Load Packages
+  #-----------------------------------------------------------------xxx-----------------------------------------------------------------
+
+  import random
+
+  random.seed(10)
+
+  def export(self:TabularPandas, fname='export.pkl', pickle_protocol=2):
+      "Export the contents of `self` without the items"
+      old_to = self
+      self = self.new_empty()
+      with warnings.catch_warnings():
+          warnings.simplefilter("ignore")
+          pickle.dump(self, open(Path(fname), 'wb'), protocol=pickle_protocol)
+          self = old_to
+
+  def get_model_performance(df_test):
+      # df_test[['fineline_nbr', 'year', 'week', 'unit_qty', 'Sales', 'pred']].tail()
+      df_test = df_test[~df_test['baseline_forecast'].isna()].copy()
+
+      MAD_baseline = int(mean_absolute_error(df_test['unit_qty'], df_test['baseline_forecast']))
+      MAD_LY = int(mean_absolute_error(df_test['unit_qty'], df_test['LY_unit_qty']))
+      MAD_DL = int(mean_absolute_error(df_test['unit_qty'], df_test['Sales']))
+      MAD_pred = int(mean_absolute_error(df_test['unit_qty'], df_test['pred']))
+      MAD_pred_all = int(mean_absolute_error(df_test['unit_qty'], df_test['pred_comb']))
+
+      print('Model Accuracy Results ...')
+      print(f"MAD_baseline: {MAD_baseline}, MAD_LY: {MAD_LY}, MAD_DL: {MAD_DL}, MAD_pred: {MAD_pred}, MAD_pred_all: {MAD_pred_all}")
+      print(f"Model Accuracy Improvement for DL Model: {int(100*(1 - MAD_DL/MAD_baseline))}%")
+      print('#-----------------------------------------------------------------xxx-----------------------------------------------------------------')
+
+  X = []
+  for i in range(50):
+    X.append('column'+ str(i))
+    
+
+  X.append('results__hasExploit')
+  X.append('results__hasFix')
+
+  y = 'results__maxExternalScore'
+
+  train_df, test_df = train_test_split(df, test_size=0.3, random_state=0)
+  X_train = train_df[X]
+  y_train = train_df[y]
+  X_test = test_df[X]
+  y_test = test_df[y]
+
+  for i in range(50):
+    df['column' + str(i)]= pd.to_numeric(df['column' + str(i)])
+
+  cont_names = []
+  for i in range(50):
+    cont_names.append('column'+ str(i))
+
+  dep_var = 'results__maxExternalScore'
+  cat_names = ['results__hasExploit',	'results__hasFix' ]
+  batch_size = 512 #current 512
+  cont_names = []
+
+  for i in range(50):
+    cont_names.append('column'+ str(i))
+
+  procs=[FillMissing, Categorify, Normalize]
+
+
+  train_df = train_df[cat_names + cont_names + [dep_var]].copy()
+
+
+  splits = RandomSplitter(valid_pct=0.2)(range_of(df))
+
+  to = TabularPandas(df.drop(columns=['vector_item']), procs=procs, cat_names = cat_names, cont_names = cont_names, y_names=dep_var, y_block=TransformBlock(), splits = splits)#, do_setup=True, device=None, inplace=False, reduce_memory=True)
+
+  #train_dl = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+  #valid_dl = DataLoader(validset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
+
+  #dls = to.dataloaders(bs=batch_size)
+
+  
+  dls = to.dataloaders(select_batch_size)
+
+  dls.c = 1
+  max_log_y = np.log(1.2) + np.max(df['results__maxExternalScore'])
+  y_range = (0, np.max(df['results__maxExternalScore']))
+
+  #"""##Creating Fit_With Function"""
+  st.success('Creating Fit_With Function before Optimization')
+
+  def fit_with(lr:float, wd:float, dp:float, n_layers:float, layer_1:float, layer_2:float, layer_3:float):
+
+    print(lr, wd, dp)
+    if round(n_layers) == 2:
+        layers = [round(layer_1), round(layer_2)]
+    elif int(n_layers) == 3:
+        layers = [round(layer_1), round(layer_2), round(layer_3)]
+    else:
+        layers = [round(layer_1)]
+    
+    config=tabular_config(ps=float(wd), embed_p=float(dp), y_range=y_range)
+
+    learn = tabular_learner(dls, layers=layers, loss_func=MSELossFlat(),
+                            config= config, metrics=[mean_absolute_error])
+
+    learn.fit_one_cycle(n_epoch = 30, wd = 0.2)
+
+    with learn.no_bar() and learn.no_logging():
+        learn.fit(n_epoch = 30,lr=float(lr), wd = float(wd))
+
+    MAE = float(learn.validate()[1])
+
+    return MAE
+
+  #"""##Specifying Hyperparameters and Initiating Optimization Function"""
+  st.success('Specifying Hyperparameters and Initiating Optimization Function')
+  
+  hps = {'lr': (1e-05, 1e-01),
+      'wd': (4e-4, 0.4),
+      'dp': (0.01, 0.5),
+       'n_layers': (1,3),
+       'layer_1': (50, 200),
+       'layer_2': (100, 1000),
+       'layer_3': (200, 2000)}
+  
+  optim = BayesianOptimization(
+    f = fit_with, # our fit function
+    pbounds = hps, # our hyper parameters to tune
+    verbose = 2, # 1 prints out when a maximum is observed, 0 for silent
+    random_state=1
+  )
+
+  #"""##Identifying most optimal parameter set"""
+  st.success('Identifying most optimal parameter set')
+
+  optim.maximize(n_iter=10)
+
+  st.sidebar.text(optim.max)
+
+
